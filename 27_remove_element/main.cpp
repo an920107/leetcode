02a7e000a4f9099189c9da0f457ccecc1a8dbee0
SquidSpirit <squid@squidspirit.com>
2025-02-15 00:24:19 +0800

    feat: 27_remove_element

---
+++ 27_remove_element/main.cpp
@@ -0,0 +1,21 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    int removeElement(vector<int>& nums, int val) {
+        if (nums.size() == 0) {
+            return 0;
+        }
+        
+        int k = 0, lastNum = nums[0];
+
+        for (int i = 0; i < nums.size(); i++) {
+            if (nums[i] != val) {
+                nums[k++] = nums[i];
+            }
+        }
+
+        return k;
+    }
+};
