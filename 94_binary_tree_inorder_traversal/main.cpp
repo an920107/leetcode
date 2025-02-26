a66d6116b3f28612e383e43172183e41742dda2c
SquidSpirit <squid@squidspirit.com>
2025-02-27 00:06:58 +0800

    feat: 94_binary_tree_inorder_traversal

---
+++ 94_binary_tree_inorder_traversal/main.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   public:
+    vector<int> inorderTraversal(TreeNode *root) {
+        vector<int> result;
+        unordered_map<TreeNode *, bool> visited;
+
+        function<void(TreeNode *)> dfs = [&](TreeNode *root) {
+            if (root == nullptr) return;
+            if (visited[root]) return;
+
+            visited[root] = true;
+
+            dfs(root->left);
+            result.emplace_back(root->val);
+            dfs(root->right);
+        };
+
+        dfs(root);
+
+        return result;
+    }
+};
