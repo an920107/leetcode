e07011a229349d55028d3439141bfe91f5ac09bf
SquidSpirit <squid@squidspirit.com>
2025-03-04 00:42:36 +0800

    feat: 55_jump_game

---
+++ 55_jump_game/main.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Solution {
+   public:
+    bool canJump(vector<int>& nums) {
+        int farthest = 0;
+
+        for (int i = 0; i < nums.size() && i <= farthest; i++) {
+            farthest = max(farthest, i + nums[i]);
+            if (farthest >= nums.size() - 1) return true;
+        }
+
+        return false;
+    }
+};
