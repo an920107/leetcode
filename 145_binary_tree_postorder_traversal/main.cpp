bd744934a169a53907117054e3e856b3b834db59
SquidSpirit <squid@squidspirit.com>
2025-03-26 23:49:44 +0800

    feat: 145_binary_tree_postorder_traversal

---
+++ 145_binary_tree_postorder_traversal/main.cpp
@@ -0,0 +1,33 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+struct TreeNode {
+    int val;
+    TreeNode *left;
+    TreeNode *right;
+    TreeNode() : val(0), left(nullptr), right(nullptr) {}
+    TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+    TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+};
+
+class Solution {
+   public:
+    vector<int> preorderTraversal(TreeNode *root) {
+        vector<int> result;
+        unordered_map<TreeNode *, bool> visited;
+
+        function<void(TreeNode *)> dfs = [&](TreeNode *root) {
+            if (!root) return;
+            if (visited[root]) return;
+            visited[root] = true;
+
+            dfs(root->left);
+            dfs(root->right);
+            result.emplace_back(root->val);
+        };
+
+        dfs(root);
+
+        return result;
+    }
+};
