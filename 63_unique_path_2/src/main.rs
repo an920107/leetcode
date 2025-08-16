137adf2926b631c4da320c65539ea4d8ad5a426d
SquidSpirit <squid@squidspirit.com>
2025-08-16 23:52:40 +0800

    feat: add initial implementation of unique paths with obstacles solution

---
+++ 63_unique_path_2/src/main.rs
@@ -0,0 +1,36 @@
+fn main() {
+    println!(
+        "{}",
+        Solution::unique_paths_with_obstacles(vec![vec![0], vec![1]])
+    );
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn unique_paths_with_obstacles(obstacle_grid: Vec<Vec<i32>>) -> i32 {
+        if obstacle_grid[0][0] == 1 {
+            return 0;
+        }
+
+        let m = obstacle_grid.len();
+        let n = obstacle_grid.first().unwrap().len();
+
+        let mut grid = vec![0; n];
+        grid[0] = 1;
+
+        for i in 0..m {
+            for j in 0..n {
+                if obstacle_grid[i][j] == 1 {
+                    grid[j] = 0;
+                } else {
+                    if j > 0 {
+                        grid[j] += grid[j - 1];
+                    }
+                }
+            }
+        }
+
+        *grid.last().unwrap()
+    }
+}
