9767170fb63db70798ebb8bb8eca0c77548718bf
SquidSpirit <squid@squidspirit.com>
2025-08-17 23:04:26 +0800

    feat: 206_reverse_linked_list

---
+++ 206_reverse_linked_list/src/main.rs
@@ -0,0 +1,44 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn reverse_list(head: Option<Box<ListNode>>) -> Option<Box<ListNode>> {
+        let mut arr: Vec<i32> = vec![];
+        let mut current_node = head;
+
+        while current_node != None {
+            arr.push(current_node.as_ref().unwrap().val);
+            current_node = current_node.unwrap().next;
+        }
+
+        if arr.is_empty() {
+            return None;
+        }
+
+        let mut head = Some(Box::new(ListNode::new(*arr.last().unwrap())));
+        let mut current_node = &mut head;
+
+        for &num in arr[..arr.len() - 1].iter().rev() {
+            current_node.as_mut().unwrap().next = Some(Box::new(ListNode::new(num)));
+            current_node = &mut current_node.as_mut().unwrap().next;
+        }
+
+        head
+    }
+}
