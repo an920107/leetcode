d259727b805fcae0291dd438e1dc5dca2849d8d1
SquidSpirit <squid@squidspirit.com>
2025-08-18 17:51:02 +0800

    feat: 2807_insert_greatest_common_divisors_in_linked_list

---
+++ 2807_insert_greatest_common_divisors_in_linked_list/src/main.rs
@@ -0,0 +1,63 @@
+fn main() {
+    println!("{}", Solution::gcd(10, 25));
+}
+
+struct Solution;
+
+#[derive(PartialEq, Eq, Clone, Debug)]
+pub struct ListNode {
+    pub val: i32,
+    pub next: Option<Box<ListNode>>,
+}
+
+impl ListNode {
+    #[inline]
+    fn new(val: i32) -> Self {
+        ListNode { next: None, val }
+    }
+}
+
+impl Solution {
+    pub fn insert_greatest_common_divisors(
+        mut head: Option<Box<ListNode>>,
+    ) -> Option<Box<ListNode>> {
+        let mut current = &mut head;
+
+        while let Some(node) = current {
+            if let Some(next_node) = &node.next {
+                let a = node.val;
+                let b = next_node.val;
+                let gcd = Solution::gcd(a, b);
+
+                let next = node.next.take();
+                node.next = Some(Box::new(ListNode { val: gcd, next }));
+
+                current = &mut node.next.as_mut().unwrap().next;
+            } else {
+                break;
+            }
+        }
+
+        head
+    }
+
+    fn gcd(mut a: i32, mut b: i32) -> i32 {
+        if a > b {
+            let tmp = a;
+            a = b;
+            b = tmp;
+        }
+
+        loop {
+            let rem = b % a;
+            b = a;
+            a = rem;
+
+            if rem == 0 {
+                break;
+            }
+        }
+
+        b
+    }
+}
