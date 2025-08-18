b1d019a05d2eae3944305f512116f19ea4346282
SquidSpirit <squid@squidspirit.com>
2025-08-18 11:12:23 +0800

    feat: 342_power_of_four

---
+++ 342_power_of_four/src/main.rs
@@ -0,0 +1,25 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn is_power_of_four(mut n: i32) -> bool {
+        if n <= 0 {
+            return false;
+        }
+
+        while n > 0 {
+            if n == 1 {
+                return true
+            }
+            if n % 4 != 0 {
+                return false;
+            }
+            n /= 4;
+        }
+
+        false
+    }
+}
