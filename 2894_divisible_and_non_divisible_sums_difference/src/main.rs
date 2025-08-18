476faf16f8993a2ef048f1a9a0f1facba34c87de
SquidSpirit <squid@squidspirit.com>
2025-08-18 18:10:42 +0800

    feat: 2894_divisible_and_non_divisible_sums_difference

---
+++ 2894_divisible_and_non_divisible_sums_difference/src/main.rs
@@ -0,0 +1,14 @@
+fn main() {
+    println!("Hello, world!");
+}
+
+struct Solution;
+
+impl Solution {
+    pub fn difference_of_sums(n: i32, m: i32) -> i32 {
+        let (dividable, undividable): (Vec<i32>, Vec<i32>) =
+            (1..(n + 1)).partition(|&val| val % m == 0);
+
+        undividable.iter().sum::<i32>() - dividable.iter().sum::<i32>()
+    }
+}
