6992ebcd5b3b4af99665e74003f427295e161447
SquidSpirit <squid@squidspirit.com>
2026-02-08 23:15:03 +0800

    1653_minimum_deletions_to_make_string_balanced using stack

--- 1653_minimum_deletions_to_make_string_balanced/src/lib.rs
+++ 1653_minimum_deletions_to_make_string_balanced/src/lib.rs
@@ -2,23 +2,21 @@ pub struct Solution;
 
 impl Solution {
     pub fn minimum_deletions(s: String) -> i32 {
-        let mut a_counts = vec![0; s.len() + 1];
+        let mut stack: Vec<u8> = vec![];
 
-        for (index, c) in s.bytes().enumerate() {
-            if c == 'a' as u8 {
-                a_counts[index + 1] = a_counts[index] + 1;
+        let mut delete_count = 0;
+        for c in s.bytes() {
+            if c == b'a'
+                && let Some(&top) = stack.last()
+                && top == b'b'
+            {
+                stack.pop();
+                delete_count += 1;
             } else {
-                a_counts[index + 1] = a_counts[index];
+                stack.push(c);
             }
         }
 
-        let mut result = i32::MAX;
-        for index in 0..(s.len() + 1) {
-            let left_b_count = index - a_counts[index];
-            let right_a_count = a_counts.last().unwrap() - a_counts[index];
-            result = result.min((left_b_count + right_a_count) as i32);
-        }
-
-        result
+        delete_count
     }
 }
