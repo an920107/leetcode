5905b9d864f395f0ed0bb328e521ee36789aff2e
SquidSpirit <squid@squidspirit.com>
2026-02-14 01:21:04 +0800

    3714_longest_balanced_substring_ii with spectial sol

--- 3714_longest_balanced_substring_ii/src/lib.rs
+++ 3714_longest_balanced_substring_ii/src/lib.rs
@@ -2,97 +2,41 @@ pub struct Solution;
 
 use std::collections::HashMap;
 
+// generate 32-bit prime by `openssl prime -generate -bits 32`
+const W1: i64 = 4031870903;
+const W2: i64 = 3649503757;
+
 impl Solution {
     pub fn longest_balanced(s: String) -> i32 {
-        let one = Solution::one(&s);
-        let two = Solution::two(&s, b'a')
-            .max(Solution::two(&s, b'b'))
-            .max(Solution::two(&s, b'c'));
-        let three = Solution::three(&s);
+        let weights: Vec<Vec<i64>> = vec![
+            vec![0, W1, W2],        // a
+            vec![W1, 0, W2],        // b
+            vec![W1, W2, 0],        // c
+            vec![W1, -W1, W2],      // ab
+            vec![W1, W2, -W1],      // ac
+            vec![W2, W1, -W1],      // bc
+            vec![W1, W2, -W1 - W2], // abc
+        ];
 
-        one.max(two).max(three)
+        weights
+            .iter()
+            .map(|w| Solution::get_max_length_for_weights(&s, &w))
+            .max()
+            .unwrap_or(0)
     }
 
-    fn one(s: &str) -> i32 {
-        let mut last_c = s.as_bytes()[0];
-        let mut result = 1;
-        let mut current_len = 1;
+    fn get_max_length_for_weights(s: &str, char_weights: &[i64]) -> i32 {
+        let mut first_seen: HashMap<i64, usize> = HashMap::from([(0, 0)]);
+        let mut current_hash = 0i64;
+        let mut result = 0;
 
-        for c in s.bytes().skip(1) {
-            if last_c == c {
-                current_len += 1;
-                result = result.max(current_len);
+        for (index, c) in s.bytes().enumerate() {
+            current_hash += char_weights[(c - b'a') as usize];
+            if let Some(last_index) = first_seen.get(&current_hash) {
+                result = result.max(index + 1 - last_index);
             } else {
-                last_c = c;
-                current_len = 1;
+                first_seen.insert(current_hash, index + 1);
             }
-        }
-
-        result
-    }
-
-    fn two(s: &str, except_c: u8) -> i32 {
-        let mut counts: Vec<Vec<i32>> = vec![vec![0; 3]];
-        let mut relations: HashMap<i32, usize> = HashMap::from([(0, 0)]);
-
-        let mut result = 0;
-
-        for (index, c) in s.bytes().enumerate() {
-            if c == except_c {
-                counts = vec![vec![0; 3]];
-                relations = HashMap::from([(0, index + 1)]);
-                continue;
-            }
-
-            counts.push(counts.last().unwrap().clone());
-            counts.last_mut().unwrap()[(c - b'a') as usize] += 1;
-
-            let last_count = counts.last().unwrap();
-            let relation = match except_c {
-                b'a' => last_count[1] - last_count[2],
-                b'b' => last_count[0] - last_count[2],
-                _ => last_count[0] - last_count[1],
-            };
-            if !relations.contains_key(&relation) {
-                relations.insert(relation, index + 1);
-            }
-
-            let current_len = (index + 1) - relations.get(&relation).unwrap_or(&(index + 1));
-            result = result.max(current_len);
-        }
-
-        result as i32
-    }
-
-    fn three(s: &str) -> i32 {
-        let mut has_seen = 0; // 7 if all of them has been seen
-        let mut counts: Vec<Vec<i32>> = vec![vec![0; 3]];
-        let mut relations: HashMap<(i32, i32), usize> = HashMap::from([((0, 0), 0)]);
-
-        let mut result = 0;
-
-        for (index, c) in s.bytes().enumerate() {
-            has_seen |= match c {
-                b'a' => 1,
-                b'b' => 2,
-                _ => 4,
-            };
-
-            counts.push(counts.last().unwrap().clone());
-            counts.last_mut().unwrap()[(c - b'a') as usize] += 1;
-
-            let last_count = counts.last().unwrap();
-            let relation = (last_count[0] - last_count[1], last_count[0] - last_count[2]);
-            if !relations.contains_key(&relation) {
-                relations.insert(relation, index + 1);
-            }
-
-            if has_seen != 7 {
-                continue;
-            }
-
-            let current_len = (index + 1) - relations.get(&relation).unwrap_or(&(index + 1));
-            result = result.max(current_len);
         }
 
         result as i32
