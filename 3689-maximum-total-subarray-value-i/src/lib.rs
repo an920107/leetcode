8d9bd9bee302017e5240de4d71282cf3d834de3a
SquidSpirit <squid@squidspirit.com>
2026-06-10 22:54:57 +0800

    3689-maximum-total-subarray-value-i

---
+++ 3689-maximum-total-subarray-value-i/src/lib.rs
@@ -0,0 +1,7 @@
+pub struct Solution;
+
+impl Solution {
+    pub fn max_total_value(nums: Vec<i32>, k: i32) -> i64 {
+        (nums.iter().max().unwrap() - nums.iter().min().unwrap()) as i64 * k as i64
+    }
+}
